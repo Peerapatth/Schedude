8d7e1242102e9790dc47edf53c6dd3f86c0e6a62
Peerapat <peerapad.xiv@mail.kmutt.ac.th>
2024-02-02 10:51:46 +0700

    Update

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -4,21 +4,27 @@
 <div class="container">
   <div class="Left">
     <div class="Left-Top">
-    <div class="Date">
-      <div class="Time">
-        <h1>05 : 00 AM</h1>
+      <div class="Date">
+        <div class="Time">
+          <h1>05 : 00 AM</h1>
+        </div>
+        <div class="Day">
+          <h3>Thuesday, 20th July</h3>
+        </div>
       </div>
-      <div class="Day">
-        <h2>Thuesday, 20th July</h2>
+      <div class="Temp">
+        <div class="City"><h3>Bangkok</h3></div>
+        <div class="Temperature"><h1>30°</h1></div>
       </div>
     </div>
-    <div class="Temp">
-      <div class="City">Weather</div>
-      <div class="Temperature">Temperature</div>
-    </div></div>
-    <div class="TodaySchedule">Today's Schedule</div>
+    <div class="TodaySchedule">
+      <div class="Schedule">
+        <h3>TODAY'S SCHEDULE</h3>
+      </div>
+    </div>
   </div>
   <div class="Right">Event</div>
+  <div class="TodayScheduleEven"></div> 
 </div>
 
 <style>
@@ -27,6 +33,18 @@
     padding: 0px 5% 0px 5%;
     display: grid;
     grid-template-columns: 1fr 1fr;
+    gap : 32px;
+  }
+
+  h1 {
+    font-size: var(--XL);
+  }
+  h2 {
+    font-size: var(--L);
+  }
+
+  h3 {
+    font-size: var(--M);
   }
 
   .Left {
@@ -36,20 +54,41 @@
   .Left-Top {
     display: flex;
     flex-direction: row;
+    justify-content: center;
     gap: 10px;
   }
 
   .Date {
-    width: 50%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     text-align: start;
     outline: 1px solid var(--bck);
-    padding: 10px;
+    padding: 15px;
     border-radius: 10px;
   }
 
-  .Time {
+  .Temp {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--bck);
+    color: var(--wht);
+    padding: 15px;
+    border-radius: 10px;
+  }
+
+  .TodaySchedule {
+    margin-top: 16px;
+  }
+
+  .Schedule {
+    padding-bottom: 6px;
+    border-bottom: 5px solid var(--bck);
+  }
+
+  .TodayScheduleEven{
+
   }
 </style>
